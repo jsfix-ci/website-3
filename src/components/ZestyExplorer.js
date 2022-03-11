7f7ff291f14a3616933b2eca871bff52f5d9d1ca
darwin apolinario <darwin.apolinario12>
2022-03-11 17:17:57 +0800

    zesty explorer search fixes

--- src/components/ZestyExplorer.js
+++ src/components/ZestyExplorer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactJson from 'react-json-view-ssr';
+import Fuse from 'fuse.js';
 
 const convertToArray = (content) =>
   Object.entries(content).map((e, i) => {
@@ -41,80 +42,60 @@ const ZestyExplorer = ({ content }) => {
   const [search, setSearch] = React.useState();
   const flaten1 = flattenObj(content);
   const flaten2 = convertToArray(flaten1);
-  const flaten3 = flaten2.find((e) => {
-    const obj123 = Object.values(e);
-    console.log(obj123, 'qqqqqqqqqqq');
-    if (Object.keys(e)[0].includes(search)) {
-      return e;
-    }
-    return null;
+  const columns = flaten2.map((e) => {
+    const res = Object.keys(e);
+    return res.toString().replace(/.[0-9]/g, '');
   });
 
-  //   const content1 = Object.entries(content).map((e, i) => {
-  //     return { [`${e[0]}`]: e[1] };
-  //   });
-  //   const searchKey1 = content1.find((e, i, arr) => {
-  //     return e[search];
-  //   });
-  console.log(deepen(flaten3), 'gggggg');
-  const deepen1 = deepen(flaten3);
-  const data = { search: search ? deepen1 : content };
-  //   const searchKey2 = content1.map((e, i, arr) => {
-  //     if (e[search]) {
-  //       return e[search];
-  //     }
-  //     if (
-  //       typeof Object.values(e)[0] === 'string' &&
-  //       Object.values(e)[0] === search
-  //     )
-  //       console.log(typeof Object.values(e)[0], 9999999999);
-  //     return 'darwin';
-  //   });
+  const options = {
+    includeScore: true,
+    useExtendedSearch: true,
+    includeMatches: true,
+    ignoreLocation: true,
+    // threshold: 0.9,
+    findAllMatches: true,
+    threshold: 0,
+    isCaseSensitive: false,
+    minMatchCharLength: 3,
+    keys: columns,
+  };
 
-  //   console.log(searchKey2, 3333333333333333333333333);
-  //   console.log(flaten3, 2222222222222222222);
-  //   const recursivelyFindKeyValue = (key, keyValue, list) => {
-  //     console.log('Searching list: ', list);
+  const fuse = new Fuse([content], options);
 
-  //     for (let i = 0; i < list.length; i++) {
-  //       const item = list[i];
+  const result = fuse.search(search || '');
 
-  //       for (const key of Object.keys(item)) {
-  //         //check if its array of more options, search it
-  //         if (Array.isArray(item[key])) {
-  //           console.log('child array found, searching', item);
-  //           const res = recursivelyFindKeyValue(key, keyValue, item[key]);
-  //           if (res.found === true) return res;
-  //         }
-  //         //Test the keyValue
-  //         else if (item[key] === keyValue) {
-  //           //found, return the list
-  //           console.log('found ', keyValue);
-  //           return { found: true, containingArray: list };
-  //         }
-  //       }
-  //     }
-
-  //     return { found: false, containingArray: [] };
-  //   };
-  //   const res = recursivelyFindKeyValue('type', 'images', content1);
-
+  const result2 =
+    result &&
+    result[0]?.matches
+      ?.map((e) => {
+        return { [`${e.key}`]: e.value };
+      })
+      .map((e) => deepen(e));
+  console.log(columns, 'COLUMNS');
+  console.log(result, '88888888888888888');
+  console.log(result2, '88888888888888888');
+  const data = search ? result2 : { content };
   return (
     <>
-      <input
-        placeholder="search for Key..........."
-        type="text"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      <ReactJson
-        src={data}
-        theme="monokai"
-        collapsed={false}
-        displayObjectSize
-        displayDataTypes={false}
-        enableClipboard={false}
-      />
+      <div style={{ width: '80vw', margin: '0 auto' }}>
+        <input
+          style={{ margin: '0 auto', width: '100%', height: '5rem' }}
+          placeholder="search"
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        {/* {JSON.stringify(result2)} */}
+        <ReactJson
+          name={'data'}
+          src={data}
+          theme="monokai"
+          collapsed={false}
+          displayObjectSize
+          displayDataTypes={false}
+          enableClipboard={true}
+        />
+      </div>
     </>
   );
 };
