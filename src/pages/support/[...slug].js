ee8faf98950a569e5ff11f23b602c1116564b5e7
Bobbi Tarkany <bl.tarkany@gmail.com>
2022-06-22 10:29:46 -0700

    udated support portal slug.js

--- src/pages/support/[...slug].js
+++ src/pages/support/[...slug].js
@@ -1,8 +1,17 @@
 import React from "react";
 
+// cookie access
+import { getUserAppSID } from "utils";
+import { getCookie } from "cookies-next";
+
+// MUI imports 
+import { useTheme } from '@mui/material/styles';
+
 
 
 export default function Support({content}){
+
+
 
     return(
         <>
